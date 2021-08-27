719c520a1c4afd6683a682663dd24518b0a61e44
AaronW <aw9994@cs.ship.edu>
2021-08-27 14:14:36 -0400

    Add custom telescope commands to dashboard

--- .config/nvim/lua/configure-dashboard.lua
+++ .config/nvim/lua/configure-dashboard.lua
@@ -1,15 +1,6 @@
 local g = vim.g
 
 g.dashboard_custom_header = {
-' ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗',
-' ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║',
-' ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║',
-' ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║',
-' ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║',
-' ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝',
-}
-
-g.dashboard_custom_header = {
 '⢀⣠⣾⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⠀⣠⣤⣶⣶',
 '⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⠀⠀⠀⢰⣿⣿⣿⣿',
 '⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣿⣧⣀⣀⣾⣿⣿⣿⣿',
@@ -24,6 +15,21 @@ g.dashboard_custom_header = {
 '⣿⣿⣿⣿⣿⣿⣿⣿⡀⠉⠀⠀⠀⠀⠀⢄⠀⢀⠀⠀⠀⠀⠉⠉⠁⠀⠀⣿⣿⣿',
 '⣿⣿⣿⣿⣿⣿⣿⣿⣧⠀⠀⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢹⣿⣿',
 '⣿⣿⣿⣿⣿⣿⣿⣿⣿⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢸⣿⣿',
+}
+
+vim.g.dashboard_custom_section = {
+    session = {
+      description = { '  Load Last Session   <leader>sl' },
+      command = 'SessionLoad',
+    },
+    recents = {
+      description = { '  Search Recent Files         fh' },
+      command = 'lua require("telescope.builtin").oldfiles()',
+    },
+    files = {
+      description = { '  Search Files                ff' },
+      command = 'lua require("telescope.builtin").find_files()',
+    }
 }
 
 g.dashboard_default_executive = 'telescope'
