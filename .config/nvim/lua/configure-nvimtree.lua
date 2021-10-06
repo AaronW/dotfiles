fcf6a5ddc9dcbb9b28389d10ed2f32987607037f
AaronW <aw9994@cs.ship.edu>
2021-10-06 18:06:31 -0400

    Update nvim-tree config to new format

--- .config/nvim/lua/configure-nvimtree.lua
+++ .config/nvim/lua/configure-nvimtree.lua
@@ -1,8 +1,6 @@
 local g = vim.g
 local tree_cb = require('nvim-tree.config').nvim_tree_callback
 
-g.nvim_tree_side = 'left'
-g.nvim_tree_width = 40
 --g.nvim_tree_auto_open = 1
 g.nvim_tree_auto_close = 1
 g.nvim_tree_follow = 1
@@ -12,15 +10,27 @@ g.nvim_tree_gitignore = 1
 g.nvim_tree_add_trailing = 1
 g.nvim_tree_ignore = { '.git', 'node_modules', '.cache', '.DS_Store', '.netrwhist', 'dist' }
 g.nvim_tree_special_files = {}
-g.nvim_tree_lsp_diagnostics = 1
 g.nvim_tree_auto_ignore_ft = { 'fzf', 'term', 'terminal' }
-g.nvim_tree_disable_default_keybindings = 1
-g.nvim_tree_bindings = {
-  { key = {'<CR>', 'o', '<2-LeftMouse>'}, cb = tree_cb('edit') },
-  { key = '<C-v>',                        cb = tree_cb('vsplit') },
-  { key = 'R',                            cb = tree_cb('refresh') },
-  { key = 'a',                            cb = tree_cb('create') },
-  { key = 'd',                            cb = tree_cb('remove') },
-  { key = 'r',                            cb = tree_cb('rename') },
-  { key = '.',                            cb = tree_cb('cd') },
+-- g.nvim_tree_disable_default_keybindings = 1
+
+local tree_cb = require('nvim-tree.config').nvim_tree_callback
+
+require'nvim-tree'.setup {
+  lsp_diagnostics = true,
+  view = {
+    width = 40,
+    auto_resize = false,
+    mappings = {
+      custom_only = true,
+      list = {
+        { key = { '<CR>', 'o', '<2-LeftMouse>' }, cb = tree_cb('edit') },
+        { key = '<C-v>', cb = tree_cb('vsplit') },
+        { key = 'R', cb = tree_cb('refresh') },
+        { key = 'a', cb = tree_cb('create') },
+        { key = 'd', cb = tree_cb('remove') },
+        { key = 'r', cb = tree_cb('rename') },
+        { key = '.', cb = tree_cb('cd') },
+      },
+    },
+  },
 }
