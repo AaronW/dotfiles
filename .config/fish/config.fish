44bdb0cfc8085e633f9e9db4433ae3fd97c729db
AaronW <aw9994@cs.ship.edu>
2021-10-11 12:18:13 -0400

    Update fish /Users/aaron/.nvm/versions/node/v14.17.0/bin:/Users/aaron/Repos/1p/webbook/scripts:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/usr/local/MacGPG2/bin:/Applications/kitty.app/Contents/MacOS configs

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,8 +4,8 @@ end
 
 export EDITOR="nvim"
 
-set PATH $PATH "/Users/aaron/Repos/1p/webbook/scripts"
-set PATH $PATH "/Users/aaron/.nvm/versions/node/v14.17.0/bin"
+fish_add_path "$HOME/Repos/1p/webbook/scripts"
+fish_add_path "$HOME/aaron/.nvm/versions/node/v14.17.0/bin"
 
 source $HOME/.config/fish/check-globals.fish
 source $HOME/.config/fish/aliases.fish
