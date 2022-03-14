5161786bd018b94541af58af88aad64fe1802560
AaronW <aw9994@cs.ship.edu>
2022-03-14 12:08:43 -0400

    Add bass to resolve help resolve nvm

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -5,9 +5,16 @@ end
 export EDITOR="nvim"
 
 fish_add_path "$HOME/Repos/1p/webbook/scripts"
-fish_add_path "$HOME/aaron/.nvm/versions/node/v14.17.0/bin"
+fish_add_path --path "$HOME/aaron/.nvm/versions/node/v14.17.0/bin"
 
 source $HOME/.config/fish/check-globals.fish
 source $HOME/.config/fish/aliases.fish
 
 starship init fish | source
+
+function nvm
+   bass source (brew --prefix nvm)/nvm.sh --no-use ';' nvm $argv
+end
+
+set -x NVM_DIR ~/.nvm
+nvm use default --silent
