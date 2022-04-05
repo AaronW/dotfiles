a3897acd67cb1d50d498f2c3acf80bf8d47a7b19
AaronW <aw9994@cs.ship.edu>
2022-04-05 15:19:56 -0400

    Remove outdated configs for lualine and indent-blankline

--- .config/nvim/lua/configure-indent-blankline.lua
+++
@@ -1,9 +0,0 @@
-require('indent_blankline').setup({
-  buftype_exclude = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'},
-  filetype_exclude = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'},
-  context_patterns = {'class', 'function', 'method', 'block', 'list_literal', 'selector', '^if', '^table', 'if_statement', 'while', 'for', 'object', 'start_tag', 'open_tag', 'element'},
-  show_first_indent_level = true,
-  show_current_context = true,
-  show_trailing_blankline_indent = false,
-})
-
