817db1e4cabfaacf0fbc2c3a410d3e03487697b3
AaronW <aw9994@cs.ship.edu>
2022-04-19 13:01:19 -0400

    Update brew config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -4,6 +4,8 @@ end
 
 export EDITOR="nvim"
 
+set -x HOMEBREW_NO_ANALYTICS 1
+
 fish_add_path "$HOME/Repos/1p/webbook/scripts"
 fish_add_path --path "$HOME/aaron/.nvm/versions/node/v14.17.0/bin"
 fish_add_path "$HOME/.cargo/bin"
