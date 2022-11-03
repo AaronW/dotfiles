60bdb8a0f134b53ea568c3d129738665bf9df683
AaronW <aw9994@cs.ship.edu>
2022-11-03 14:07:05 -0400

    Update lsp configuration

--- .config/nvim/lua/lsp/css.lua
+++ .config/nvim/lua/lsp/css.lua
@@ -1,7 +1,7 @@
 -- Enable (broadcasting) snippet capability for completion
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
-capabilities = require('cmp_nvim_lsp').update_capabilities(capabilities)
+capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
 require('lspconfig').cssls.setup({
   on_attach = require('lsp.utils').on_attach,
--- .config/nvim/lua/lsp/lua.lua
+++ .config/nvim/lua/lsp/lua.lua
@@ -15,7 +15,7 @@ if string.find(vim.loop.cwd(), 'hammerspoon') then
 end
 
 require('lspconfig').sumneko_lua.setup({
-  capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities()),
+  capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities()),
   cmd = { bin_root .. 'lua-language-server', '-E', base_root .. '/libexec/main.lua' },
   on_attach = require('lsp.utils').on_attach,
   root_dir = require('lspconfig.util').root_pattern('.git', '.luacheckrc', 'stylua.toml'),
