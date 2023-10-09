5739fd7eb3fc6de52b94a03db6d645deb26ed28d
AaronW <aw9994@cs.ship.edu>
2023-10-09 13:47:27 -0400

    Update indent-blankline to use v3 configuration syntax

--- .config/nvim/lua/configure/indent-blankline.lua
+++ .config/nvim/lua/configure/indent-blankline.lua
@@ -1,13 +1,15 @@
 return {
   'lukas-reineke/indent-blankline.nvim',
+  main = "ibl", opts = {},
   config = function()
-    require('indent_blankline').setup({
-      buftype_exclude = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'},
-      filetype_exclude = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'},
-      context_patterns = {'class', 'function', 'method', 'block', 'list_literal', 'selector', '^if', '^table', 'if_statement', 'while', 'for', 'object', 'start_tag', 'open_tag', 'element'},
-      show_first_indent_level = true,
-      show_current_context = true,
-      show_trailing_blankline_indent = false,
+    require('ibl').setup({
+      exclude = {
+        buftypes = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'},
+        filetypes = {'NvimTree', 'terminal', 'term', 'packer', 'dashboard', 'gitcommit', 'fugitive'}
+      },
+      -- context_patterns = {'class', 'function', 'method', 'block', 'list_literal', 'selector', '^if', '^table', 'if_statement', 'while', 'for', 'object', 'start_tag', 'open_tag', 'element'},
+      -- show_first_indent_level = true,
+      -- show_trailing_blankline_indent = false,
     })
   end,
 }
